dad7c57ecf36074ccbdcc094e4d081fcec109080
Michael Spitznagel <exultavit@gmail.com>
2022-12-02 10:34:44 -0500

    Solve part one

--- 02/day02.py
+++ 02/day02.py
@@ -2,11 +2,43 @@
 
 from aocd.models import Puzzle
 
+ROCK = 1
+PAPER = 2
+SCISSORS = 3
+
+SHAPE_CODE = {"A": ROCK,
+              "B": PAPER,
+              "C": SCISSORS,
+              "X": ROCK,
+              "Y": PAPER,
+              "Z": SCISSORS}
+
+DEFEATER_OF = {ROCK: PAPER,
+               PAPER: SCISSORS,
+               SCISSORS: ROCK}
+
+
+def round_score_a(input_line: str) -> int:
+    """Calculate the score for one round in part a."""
+
+    opponent_shape = SHAPE_CODE[input_line[0]]
+    player_shape = SHAPE_CODE[input_line[2]]
+
+    score = player_shape
+    if DEFEATER_OF[opponent_shape] == player_shape:
+        score += 6
+    elif DEFEATER_OF[player_shape] == opponent_shape:
+        pass
+    else:
+        score += 3
+
+    return score
+    
 
 def part_a(input_data: str) -> int:
     """Given the puzzle input data, return the solution for part A."""
 
-    return "Solution not implemented"
+    return sum(round_score_a(line) for line in input_data.split("\n"))
 
 
 def part_b(input_data: str) -> int:
