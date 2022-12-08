8c56648f056f780ac288bb70c019a091c64b8bb1
Michael Spitznagel <exultavit@gmail.com>
2022-12-08 14:07:23 -0500

    Add day 04 and test for sample part one result

---
+++ 04/test_04.py
@@ -0,0 +1,25 @@
+"""Tests for day 04 of Advent of Code 2022."""
+
+import day04
+
+# Test data given as a multiline string.
+sample_input_data = """2-4,6-8
+2-3,4-5
+5-7,7-9
+2-8,3-7
+6-6,4-6
+2-6,4-8"""
+
+sample_solution_a = 2
+
+sample_solution_b = "Sample solution not entered"
+
+
+def test_part_a():
+    """Test the solution on sample data for part A."""
+    assert day04.part_a(sample_input_data) == sample_solution_a
+
+
+def test_part_b():
+    """Test the solution on sample data for part B."""
+    assert day04.part_b(sample_input_data) == sample_solution_b
