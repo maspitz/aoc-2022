a4939fd5c7fe8400a8d54f692bc90951bcc44666
Michael Spitznagel <exultavit@gmail.com>
2022-12-08 14:54:28 -0500

    Solve part one

--- 04/day04.py
+++ 04/day04.py
@@ -2,10 +2,38 @@
 
 from aocd.models import Puzzle
 
+from parse import parse
+
+def parse_assignment_pair(line: str) -> tuple:
+    """Parses an input line, returning a pair of assigned ranges.
+
+    Ranges are in the form (lo, hi), where lo <= hi.
+
+    Example:
+           parse_assignment_pair("1-4,2-6")
+    Returns:
+           ((1, 4), (2, 6))"""
+
+    p = parse("{:d}-{:d},{:d}-{:d}", line)
+    if p == None:
+        raise(f"Could not parse line: {line}")
+    a_lo, a_hi, b_lo, b_hi = p.fixed
+    if a_lo > a_hi or b_lo > b_hi:
+        raise(f"Incorrect ordering of range in line: {line}")
+    return ((a_lo, a_hi), (b_lo, b_hi))
+
 def part_a(input_data: str) -> int:
     """Given the puzzle input data, return the solution for part A."""
 
-    return "Solution not implemented"
+    assignments = [parse_assignment_pair(line)
+                   for line in input_data.split('\n')]
+
+    fully_contained = sum((a_lo <= b_lo and b_hi <= a_hi) or
+                          (b_lo <= a_lo and a_hi <= b_hi)
+                          for ((a_lo, a_hi), (b_lo, b_hi)) in assignments)
+
+    return fully_contained
+
 
 
 def part_b(input_data: str) -> int:
