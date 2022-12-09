8348faa8cfd1248aca3dc08e036c832bee7f5ea1
Michael Spitznagel <exultavit@gmail.com>
2022-12-09 10:58:13 -0500

    Solve part two

--- 05/day05.py
+++ 05/day05.py
@@ -40,8 +40,10 @@ def parse_stacks(stacks_data: str) -> list[list[str]]:
     return stacks
 
 
-def move_crates(stacks: list[list[str]], moves: list[str]) -> None:
-    """Modifies the stacks list by applying the specified crate moves."""
+def move_single_crates(stacks: list[list[str]], moves: list[str]) -> None:
+    """Modifies the stacks list by applying the specified moves to individual crates.
+
+    This corresponds to the behavior of the CrateMover 9000 of part one."""
 
     for move in moves:
         parsed_move = parse('move {:d} from {:d} to {:d}', move)
@@ -50,9 +52,33 @@ def move_crates(stacks: list[list[str]], moves: list[str]) -> None:
 
         # Note: in the move specifications, stacks are indexed starting at 1
         num_crates, src, dst = parsed_move
+        src -= 1
+        dst -= 1
+
         for _ in range(num_crates):
-            crate = stacks[src-1].pop()
-            stacks[dst-1].append(crate)
+            crate = stacks[src].pop()
+            stacks[dst].append(crate)
+
+    return None
+
+
+def move_multiple_crates(stacks: list[list[str]], moves: list[str]) -> None:
+    """Modifies the stacks list by applying the specified moves to multiple crates.
+
+    This corresponds to the behavior of the CrateMover 9001 of part two."""
+
+    for move in moves:
+        parsed_move = parse('move {:d} from {:d} to {:d}', move)
+        if parsed_move == None:
+            raise ValueError("Could not parse move: {move}")
+
+        # Note: in the move specifications, stacks are indexed starting at 1
+        num_crates, src, dst = parsed_move
+        src -= 1
+        dst -= 1
+
+        stacks[dst].extend(stacks[src][-num_crates:])
+        stacks[src] = stacks[src][:-num_crates]
 
     return None
 
@@ -64,7 +90,7 @@ def part_a(input_data: str) -> int:
     stacks = parse_stacks(stack_data)
     moves = move_data.split('\n')
 
-    move_crates(stacks, moves)
+    move_single_crates(stacks, moves)
 
     # Note: If any stack is empty at this point, this will raise an IndexError
     stack_tops = [stack[-1] for stack in stacks]
@@ -75,7 +101,16 @@ def part_a(input_data: str) -> int:
 def part_b(input_data: str) -> int:
     """Given the puzzle input data, return the solution for part B."""
 
-    return "Solution not implemented"
+    stack_data, move_data = input_data.split('\n\n')
+    stacks = parse_stacks(stack_data)
+    moves = move_data.split('\n')
+
+    move_multiple_crates(stacks, moves)
+
+    # Note: If any stack is empty at this point, this will raise an IndexError
+    stack_tops = [stack[-1] for stack in stacks]
+
+    return "".join(stack_tops)
 
 
 if __name__ == '__main__':
