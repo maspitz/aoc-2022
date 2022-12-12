5df106e145006ab108ef0627efe971aa4a1d9696
Michael Spitznagel <exultavit@gmail.com>
2022-12-12 14:28:46 -0500

    Solve part one

--- 06/day06.py
+++ 06/day06.py
@@ -2,11 +2,26 @@
 
 from aocd.models import Puzzle
 
+from collections import deque
 
 def part_a(input_data: str) -> int:
     """Given the puzzle input data, return the solution for part A."""
 
-    return "Solution not implemented"
+    if len(input_data) < 4:
+        raise ValueError("Input data is less than four characters long.")
+
+    marker = deque(input_data[:3])
+    marker_pos = 3
+    for ch in input_data[3:]:
+        marker.append(ch)
+        marker_pos += 1
+        if len(set(marker)) == 4:
+            return marker_pos
+        else:
+            marker.popleft()
+    raise ValueError("Input data has no sequence of four consecutive "
+                     "unique characters.")
+    return None
 
 
 def part_b(input_data: str) -> int:
