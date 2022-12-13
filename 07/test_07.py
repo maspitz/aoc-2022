5444c29d4e362f4c86869b4c7e7f9d65c720e3fa
Michael Spitznagel <exultavit@gmail.com>
2022-12-13 16:33:18 -0500

    Test solution of part one on sample data

--- 07/test_07.py
+++ 07/test_07.py
@@ -3,9 +3,31 @@
 import day07
 
 # Test data given as a multiline string.
-sample_input_data = """Sample data not entered"""
+sample_input_data = "$ cd /
+$ ls
+dir a
+14848514 b.txt
+8504156 c.dat
+dir d
+$ cd a
+$ ls
+dir e
+29116 f
+2557 g
+62596 h.lst
+$ cd e
+$ ls
+584 i
+$ cd ..
+$ cd ..
+$ cd d
+$ ls
+4060174 j
+8033020 d.log
+5626152 d.ext
+7214296 k"""
 
-sample_solution_a = "Sample solution not entered"
+sample_solution_a = 95437
 
 sample_solution_b = "Sample solution not entered"
 
