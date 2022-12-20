4625c7b11da1d1d9f7cf7b6fe54eafd487302ef9
Michael Spitznagel <exultavit@gmail.com>
2022-12-20 15:26:28 -0500

    Provision day 11

---
+++ 11/test_11.py
@@ -0,0 +1,20 @@
+"""Tests for day 11 of Advent of Code 2022."""
+
+import day11
+
+# Test data given as a multiline string.
+sample_input_data = """Sample data not entered"""
+
+sample_solution_a = "Sample solution not entered"
+
+sample_solution_b = "Sample solution not entered"
+
+
+def test_part_a():
+    """Test the solution on sample data for part A."""
+    assert day11.part_a(sample_input_data) == sample_solution_a
+
+
+def test_part_b():
+    """Test the solution on sample data for part B."""
+    assert day11.part_b(sample_input_data) == sample_solution_b
